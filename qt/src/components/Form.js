0e0f1e405f78f6ce8fd7d0c9e3c243ab908cf9f0
Quan Tran <tran.q90@gmail.com>
2019-05-13 23:48:58 -0700

    ??

--- qt/src/components/Form.js
+++ qt/src/components/Form.js
@@ -1,54 +1,43 @@
 import React, { Component } from "react";
 
 class Form extends Component {
-  // Setting the initial values of this.state.username and this.state.password
   state = {
-    username: "",
-    password: ""
+    rick: false,
+    morty: false,
+    squanchy: false,
+    bill: false,
+    summer: false,
+    poopybutthole: false,
+    krombopulous: false,
+    sleepygary: false,
+    jerry: false
   };
 
-  handleInputChange = event => {
+  handleClick = event => {
     const value = event.target.value;
     const name = event.target.name;
 
     this.setState({
       [name]: value
     });
-  };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-
-    alert(`Username: ${this.state.username} Password: ${this.state.password}`);
-
-    this.setState({
-      username: "",
-      password: ""
-    });
+    alert(`Username: ${this.state.rick} `);
+    console.log(this.state.name)
   };
 
   render() {
     return (
-
-      <form>
-        <p>Username: {this.state.username}</p>
-        <p>Password: {this.state.password}</p>
-        <input
-          value={this.state.username}
-          name="username"
-          onChange={this.handleInputChange}
-          type="text"
-          placeholder="Username"
-        />
-        <input
-          value={this.state.password}
-          name="password"
-          onChange={this.handleInputChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button onClick={this.handleFormSubmit}>Submit</button>
-      </form>
+      <div>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/a/a6/Rick_Sanchez.png/revision/latest?cb=20160923150728" name="rick" value="rick" onClick={this.handleClick}></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/4/41/Morty_Smith.jpg/revision/latest?cb=20170217193441" name="morty"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/1/16/Squanchy_.png/revision/latest?cb=20160830140218" name="squanchy"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/disney/images/4/41/Profile_-_Bill_Cipher.png/revision/latest?cb=20190318132904" name="bill"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/a/ad/Summer_is_cool.jpeg/revision/latest?cb=20160919183158" name="summer"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/3/37/Mr_poopy_butthole.png/revision/latest?cb=20150819161234" name="poopybutthole"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/9/93/Krombopulos_Michael.png/revision/latest/scale-to-width-down/1000?cb=20160919070008" name="krombopulous"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/9/9c/Sleepy_Gary_Better_Image.png/revision/latest?cb=20170131021113" name="sleepygary"></img>
+        <img width="150px" height="150px" src="https://vignette.wikia.nocookie.net/rickandmorty/images/f/f1/Jerry_Smith.png/revision/latest?cb=20160923151111" name="jerry"></img>
+      </div>
     );
   }
 }
